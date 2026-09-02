57e9b178d2699eecfd96c8e89d84530e94cf58a9
Luka <bosnjakluka008@gmail.com>
2026-09-03 01:34:00 +0200

    Manga page/reading metadata completed

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def index():
     if request.method == 'POST':
         request_content = request.form['content']
         if request_content == 'one':
-                new_manga = Manga(title='Berserk', native_title='ベルセルク', type='Manga', status='Completed', year=1989, rating=9.5, description='A dark fantasy manga series written and illustrated by Kentaro Miura.', genres=['Action', 'Adventure', 'Dark Fantasy'], tags=['Guts', 'Griffith', 'Casca'], authors=['Kentaro Miura'], total_chapters=364, cover_url='https://example.com/berserk.jpg', avg_rating=9.5)
-                new_manga2 = Manga(title='Naruto', native_title='ナルト', type='Manga', status='Completed', year=1999, rating=8.5, description='A manga series written and illustrated by Masashi Kishimoto.', genres=['Action', 'Adventure', 'Fantasy'], tags=['Naruto Uzumaki', 'Ninja', 'Shinobi'], authors=['Masashi Kishimoto'], total_chapters=700, cover_url='https://example.com/naruto.jpg', avg_rating=8.5)
+                new_manga = Manga(title='Berserk', native_title='ベルセルク', type='Manga', status='Completed', year=1989, rating=9.5, description='A dark fantasy manga series written and illustrated by Kentaro Miura.', genres=['Action', 'Adventure', 'Dark Fantasy'], tags=['Guts', 'Griffith', 'Casca'], authors=['Kentaro Miura'], total_chapters=364, cover_url='https://comicbook.com/wp-content/uploads/sites/4/2024/04/9d93e8f2-9e48-408c-894f-9d7182489a57.jpg?resize=1024,576', avg_rating=9.5)
+                new_manga2 = Manga(title='Naruto', native_title='ナルト', type='Manga', status='Completed', year=1999, rating=8.5, description='A manga series written and illustrated by Masashi Kishimoto.', genres=['Action', 'Adventure', 'Fantasy'], tags=['Naruto Uzumaki', 'Ninja', 'Shinobi'], authors=['Masashi Kishimoto'], total_chapters=700, cover_url='https://m.media-amazon.com/images/I/91RpwagB7uL._SL1500_.jpg', avg_rating=8.5)
                 new_manga3 = Manga(title='Naruto 2', native_title='ナルト 2', type='Manga', status='Completed', year=2000, rating=8.0, description='A manga series written and illustrated by Masashi Kishimoto.', genres=['Action', 'Adventure', 'Fantasy'], tags=['Naruto Uzumaki', 'Ninja', 'Shinobi'], authors=['Masashi Kishimoto'], total_chapters=700, cover_url='https://example.com/naruto2.jpg', avg_rating=8.0)
         else:
                 new_manga = Manga(title='One Piece', native_title='ワンピース', type='Manga', status='Ongoing', year=1997, rating=9.0, description='A manga series written and illustrated by Eiichiro Oda.', genres=['Action', 'Adventure', 'Fantasy'], tags=['Monkey D. Luffy', 'Pirates', 'Treasure'], authors=['Eiichiro Oda'], total_chapters=1000, cover_url='https://example.com/onepiece.jpg', avg_rating=9.0)
@@ -60,7 +60,7 @@ def index():
 def search():
     if request.method == 'POST':
         request_title = request.form['query']
-        stmt = db.select(Manga.title).where(Manga.title.contains(request_title))
+        stmt = db.select(Manga).where(Manga.title.contains(request_title))
         result = db.session.execute(stmt).scalars().all()
         return render_template('search.html', result=result)
 
@@ -70,6 +70,15 @@ def search():
         return render_template('search.html')
             
 
+@app.route('/manga/<int:id>')
+def manga(id):
+    stmt = db.select(Manga).where(Manga.id == id)
+    manga = db.session.execute(stmt).scalars().first()
+
+
+    return render_template('manga.html', manga=manga, id=id)
+
+    
     
 if __name__ == "__main__": 
     app.run(debug=True)
